a019c18d67538f1ab5891281bbf635b51dae8603
chompski <tutters2@hotmail.com>
2018-07-09 12:46:21 +0100

    Added testing and more data

--- routes/api.route.ants.js
+++ routes/api.route.ants.js
@@ -5,4 +5,8 @@ const antsCont = require('../controllers/ants.controller');
 
 router.get('/', antsCont.getAnts);
 
+router.get('*', function (req, res){
+    res.send('404 Not Found', 404);
+});
+
 module.exports = router;
